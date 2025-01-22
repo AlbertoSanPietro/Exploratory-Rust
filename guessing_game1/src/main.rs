8df72e55e3e0db78ca0fd48fceb340c4717646e6
AlbertoSanPietro <albertoxsp@gmail.com>
2025-01-22 17:27:53 +0100

    Added a small Rust program, a variation of one og the earlist ones in the Rust Book

---
+++ guessing_game1/src/main.rs
@@ -0,0 +1,37 @@
+
+
+use std::io;
+use std::cmp::Ordering;
+use std::time::{SystemTime, UNIX_EPOCH};
+fn main() {
+   
+    let nanos = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().subsec_nanos();
+    let solution  = (nanos/10 /*%100*/)+1;
+    println!("Welcome to \"guess the number!\"");
+    loop {
+        println!("Insert your number:");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Unable to read line");
+        
+        if guess.trim() == "$" {
+            println!("The number is: {}", solution);
+            continue;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num)=>num,
+            Err(_) =>continue,
+        };
+        
+        println!("You tried with: {}", guess);
+    
+        match guess.cmp(&solution) {
+            Ordering::Less =>println!("Too small"),
+            Ordering::Greater =>println!("Too big"),
+            Ordering::Equal =>{
+                println!("You won!");
+                break;
+            }
+        }
+    }
+}
